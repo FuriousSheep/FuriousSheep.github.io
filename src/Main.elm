45e05c9060347526e3f55aded10d63825741b4f4
Ioannis Katsaros <katsarosia@gmail.com>
2023-11-23 23:03:47 +0100

    Init elm

---
+++ src/Main.elm
@@ -0,0 +1,24 @@
+module Main exposing (..)
+
+import Browser.Navigation exposing (Key)
+import Browser exposing (application, document)
+import Html
+import Url exposing (Url)
+
+type alias Model = { a : Int }
+
+type Msg = NoOp
+
+type alias Flags = { b : Int} 
+
+init : Flags -> Url -> Key -> (Model, Cmd Msg)
+init _ _ _ = ( {a = 0}, Cmd.none )
+
+main = application
+  { init = init
+  , onUrlChange = \_ -> NoOp
+  , onUrlRequest = \_ -> NoOp
+  , subscriptions = \ _ -> Sub.none
+  , update = \_ _ -> ({a = 0}, Cmd.none)
+  , view = \_ -> { title = "", body=  [Html.text "Under construction..."]}
+  }
